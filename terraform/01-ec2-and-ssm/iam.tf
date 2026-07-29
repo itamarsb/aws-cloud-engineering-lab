958244729ae9049393f985b911e310ddb500ad19
Itamar de Sá Britto Júnior <80598950+itamarsb@users.noreply.github.com>
2026-07-28 22:03:58 -0300

    Update iam.tf

--- terraform/01-ec2-and-ssm/iam.tf
+++ terraform/01-ec2-and-ssm/iam.tf
@@ -1,1 +1,29 @@
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    sid     = "AllowEC2AssumeRole"
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ec2_ssm" {
+  name               = "${local.resource_prefix}-role"
+  description        = "Allows the Terraform-managed Lab 01 EC2 instance to use AWS Systems Manager."
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_core" {
+  role       = aws_iam_role.ec2_ssm.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ec2_ssm" {
+  name = "${local.resource_prefix}-instance-profile"
+  role = aws_iam_role.ec2_ssm.name
+}
 
