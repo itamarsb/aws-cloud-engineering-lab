1b213f1c8ff0493105f221f499af29da316109cf
Itamar de Sá Britto Júnior <80598950+itamarsb@users.noreply.github.com>
2026-07-28 22:04:37 -0300

    Update locals.tf

--- terraform/01-ec2-and-ssm/locals.tf
+++ terraform/01-ec2-and-ssm/locals.tf
@@ -1,1 +1,16 @@
+locals {
+  resource_prefix = "lab01-ec2-ssm-tf"
+
+  ubuntu_ami_parameter = "/aws/service/canonical/ubuntu/server/${var.ubuntu_release}/stable/current/amd64/hvm/ebs-gp3/ami-id"
+
+  common_tags = {
+    Project        = var.project_name
+    Environment    = var.environment
+    Lab            = "01-ec2-and-ssm"
+    ManagedBy      = "Terraform"
+    Provisioning   = "Infrastructure-as-Code"
+    Implementation = "Parallel-Terraform"
+    Repository     = "github.com/itamarsb/aws-cloud-engineering-lab"
+  }
+}
 
