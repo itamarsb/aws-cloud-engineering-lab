b350845a30a3ef1a6d70de7529495fd4882ff59a
Itamar de Sá Britto Júnior <80598950+itamarsb@users.noreply.github.com>
2026-07-28 22:05:20 -0300

    Update network.tf

--- terraform/01-ec2-and-ssm/network.tf
+++ terraform/01-ec2-and-ssm/network.tf
@@ -1,1 +1,17 @@
+resource "aws_security_group" "ec2_ssm" {
+  name        = "${local.resource_prefix}-sg"
+  description = "No inbound access. Administrative sessions use AWS Systems Manager."
+  vpc_id      = data.aws_vpc.default.id
+
+  tags = {
+    Name = "${local.resource_prefix}-sg"
+  }
+}
+
+resource "aws_vpc_security_group_egress_rule" "https_and_updates" {
+  security_group_id = aws_security_group.ec2_ssm.id
+  description       = "Allow outbound IPv4 connectivity to AWS APIs and operating-system repositories."
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
+}
 
