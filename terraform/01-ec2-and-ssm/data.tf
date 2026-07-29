b26a1cdf032e5aeeb96a0cc5208816b095d9ec87
Itamar de Sá Britto Júnior <80598950+itamarsb@users.noreply.github.com>
2026-07-28 22:03:10 -0300

    Update data.tf

--- terraform/01-ec2-and-ssm/data.tf
+++ terraform/01-ec2-and-ssm/data.tf
@@ -1,1 +1,22 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+
+  filter {
+    name   = "default-for-az"
+    values = ["true"]
+  }
+}
+
+data "aws_ssm_parameter" "ubuntu_ami" {
+  name = local.ubuntu_ami_parameter
+}
 
